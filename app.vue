46396168b9815f9ac0301f91a9b2f2022ad62eea
Mimoun <mimounhm.97@gmail.com>
2025-01-31 00:11:02 +0100

    بِسْمِ ٱللَّهِ مَجْر۪ىٰهَا وَمُرْسَىٰهَآ

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <div class="flex-1 bg-blue-500 flex items-center justify-center">
+      <div class="text-6xl font-black italic">CIELO</div>
+    </div>
+    <div class="flex-1 bg-green-500 flex items-center justify-center">
+      <div class="text-6xl font-black italic">MONTAÑAS</div>
+    </div>
+    <div class="flex-1 bg-yellow-500 flex items-center justify-center bg-cover" style="background-image: url('');">
+      <div class="text-6xl font-black italic">ARENA</div>
+    </div>
+  </div>
+</template>
