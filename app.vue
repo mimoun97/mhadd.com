73ce6eeb46a568037eec492f3942ba88924668c6
Mimoun <mimounhm.97@gmail.com>
2025-03-21 14:11:37 +0100

    SEO: added title, image, favicon

--- app.vue
+++ app.vue
@@ -1,8 +1,3 @@
-<script setup lang="ts">
-import BentoGrid from './components/BentoGrid.vue';
-
-</script>
-
 <template>
   <main class="font-display min-h-screen bg-zinc-900 px-4 py-12 text-zinc-50">
   
@@ -23,3 +18,14 @@ import BentoGrid from './components/BentoGrid.vue';
 </svg>
 
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'mhadd.com - Desarrollador Web (App) Fullstack',
+  ogTitle: 'mhadd.com - Desarrollador Web (App) Fullstack',
+  description: 'Soy un desarrollador web especializado en crear aplicaciones personalizadas con tecnologías como Nuxt, Laravel y Tailwind. Ofrezco soluciones modernas y escalables para tu negocio.',
+  ogDescription: 'Soy un desarrollador web especializado en crear aplicaciones personalizadas con tecnologías como Nuxt, Laravel y Tailwind. Ofrezco soluciones modernas y escalables para tu negocio.',
+  ogImage: 'https://mhadd.com/images/ogImage.jpg',
+  twitterCard: 'summary_large_image',
+})
+</script>
