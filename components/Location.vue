ca6b9e9abb498a40bc05096bb938d0c72d000044
Mimoun <mimounhm.97@gmail.com>
2025-03-21 15:25:50 +0100

    UI: added location marker animated

--- components/Location.vue
+++ components/Location.vue
@@ -1,13 +1,14 @@
 <template>
     <!-- Location -->
-    <Block class="relative bg-[url(/images/Amposta.svg)] bg-cover bg-center min-h-54 col-span-12 flex flex-col items-center gap-4 md:col-span-3 bg-zinc-800">
+    <Block class="relative bg-[url(/images/Amposta.svg)] bg-cover bg-center min-h-54 col-span-12 flex justify-center items-center gap-4 md:col-span-3 bg-zinc-800">
+        <LocationMarker />
         <motion.a :href="MAP_LOCATION" target="_blank" :initial="{ opacity: 0, scale: 0 }"
     :animate="{ opacity: 1, scale: 1 }"
     :transition="{
       duration: 0.4,
       scale: { type: 'spring', visualDuration: 0.4, bounce: 0.5 }
-    }" class="block cursor-pointer rounded-xl absolute left-2 bottom-2 p-2 bg-zinc-800 outline-offset-1 outline-zinc-100/10 hover:outline-2">
-            <span class="block text-center text-sm text-zinc-400">Amposta, España</span>
+    }" class="shadow-md backdrop-blur-xs block cursor-pointer rounded-2xl absolute left-2 bottom-2 py-2 px-4 bg-zinc-800/70 hover:bg-zinc-800/60 transition-colors outline-offset-1 outline-zinc-100/10 hover:outline-2">
+            <span class="block text-center text-sm text-zinc-100">Amposta, España</span>
         </motion.a>
     </Block>
 </template>
@@ -15,6 +16,7 @@
 <script setup>
 
 import { motion } from 'motion-v'
+import LocationMarker from './LocationMarker.vue';
 
 const MAP_LOCATION = ref("https://www.google.com/maps/@40.7083287,0.5678079,15z")
 </script>
---
+++ components/LocationMarker.vue
@@ -0,0 +1,6 @@
+<template>
+    <span class="relative flex justify-center items-center size-10">
+            <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-[#679BFF] opacity-75"></span>
+            <span class="shadow-xl relative inline-flex size-7 rounded-full bg-[#679BFF] border-white border-3"></span>
+        </span>
+</template>
